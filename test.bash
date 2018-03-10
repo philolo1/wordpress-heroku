913882a47e4d0be8ccbd4fe0838b840f6844f1c7
Patrick Klitzke <philologos14@gmail.com>
2018-03-10 23:04:11 +0900

    environment variables

---
+++ test.bash
@@ -0,0 +1,9 @@
+heroku config:set AUTH_KEY='hCxi5=k[d1P22)o6#P/j-Cs|O7 F8PS,)z,@W^.Bx8$]]ecgiQ^$xrQ;BRki%oQ`' \
+  SECURE_AUTH_KEY='qAdM>`>0]o!Xc$iS$:k.-aHkZvj_R]@QNlh{qU;v(=Td6VlJS!wJLTn4-v~PAYQ.' \
+  LOGGED_IN_KEY='Hl*@TS1=6-g -^;T0SOsB=Cv28V|,#!3rD728[k+W_V}qIJ:ei}-Fd4ecsrfB#`]' \
+  NONCE_KEY='{^GAW0n|qNX.Z)L;+I#OD-;<qlx4PRU`&G9y8_;=~+#^ $s_h@C>6qpp)YvPM/?#' \
+  AUTH_SALT='>[+=Se<P3<:TP1d|v~cbel-h-dK%&&p<D$u-]!{+)-?jN:P_2kAe`|c<&7A{57k_' \
+  SECURE_AUTH_SALT='138Td`rWd2bN9k<UG7sE6x+H;%mPP.>&2}0Z<d}OSudKf?[!*+`Y!hkv~xJ UGe9' \
+  LOGGED_IN_SALT='y.wtZ=&PR=OA@XxR06x+8hxm5I$w.-YNh,bx;)arTU3@%7GV+kxL6r=-8}9RCL0R' \
+  NONCE_SALT='&j|bK;d-eeo~B@8>4h56Fo|`g4wugD@%u{-.6K`Rhz`lj)]|~3F}bShA2CZ!o1Z]'
+
